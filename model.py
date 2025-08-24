50a40cfbc95d068d67b48bda4238506ed929b55c
mahmoud1512 <mahmoud21011275@gmail.com>
2025-08-24 14:54:56 +0300

    add api key

--- model.py
+++ model.py
@@ -1,11 +1,23 @@
 
-# TODO : installing langchain and stremlit libraries
-
-
 # TODO : writing code to use the target needed model type 
 
 # TODO : use prompt template to easy access the model
+from dotenv import load_dotenv
+import os
+
+class translator():
+    # initing with the google api key
+    def __init__(self):
+        load_dotenv()
+        self.api_key=os.getenv("GOOGLE_API_KEY")
+        print(self.api_key)
+
+
 
 def run(source_lang, target_lang, source_sentence):
 
     return "hello world"
+
+
+
+x=translator()
